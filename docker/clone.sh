b9f75b9734e6d0cbddbce373115f12bbf391ebd5
Mark Olliver <mark@digitalpatterns.io>
2019-04-26 10:12:27 +0100

    updates to paths

---
+++ docker/clone.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export COP="${HOME}/GIT/cop"
+mkdir -p ${COP}
+cd ${COP}
+
+git clone git@github.com:UKHomeOffice/RefData.git public_refdata_flyway
+git clone ssh://git@gitlab.digital.homeoffice.gov.uk:2222/cop/private-refdata.git private_refdata_flyway
