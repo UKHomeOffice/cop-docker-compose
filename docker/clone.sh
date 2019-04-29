72e8543532819a1866e1aa0f9fa7f2ed8cec6f1c
Mark Olliver <mark@digitalpatterns.io>
2019-04-29 07:28:42 +0100

    updated with ui settings

--- docker/clone.sh
+++ docker/clone.sh
@@ -6,3 +6,8 @@ cd ${COP}
 
 git clone git@github.com:UKHomeOffice/RefData.git public_refdata_flyway
 git clone ssh://git@gitlab.digital.homeoffice.gov.uk:2222/cop/private-refdata.git private_refdata_flyway
+git clone ssh://git@gitlab.digital.homeoffice.gov.uk:2222/cop/private-reports.git reporting_data
+cd reporting_data
+git checkout develop
+cd ${COP}
+
