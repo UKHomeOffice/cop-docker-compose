bfd6ef92a73b9503b04f8f925041b048510018a2
Mark Olliver <mark@digitalpatterns.io>
2019-04-29 14:46:09 +0100

    update docker env

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -5,3 +5,11 @@ docker network create db
 sudo ifconfig lo0 alias 10.254.254.254
 mkdir -p proxy/acme
 touch proxy/acme/acme.json
+
+if [[ -f platform/mobileid-key.pem ]]
+then
+    echo "existing mobileID key exists"
+else
+    openssl genrsa -des3 -out platform/mobileid-key.pem 2048
+    openssl rsa -in platform/mobileid-key.pem -out mobileid-key.pem
+fi
